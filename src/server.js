1919d7b8b5feb09bc374be16e7a1cbe48ef8d396
sahanilxm <laxmisahani240@gmail.com>
2023-12-17 18:25:11 +0530

    First Commit to setup the project.

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { PORT } = require('./config/serverConfig');
+
+const setupAndRunServer = () =>{
+
+    const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended : true}));
+
+    app.listen(PORT, ()=>{
+        console.log(`Server started at port number: ${PORT}`);
+    });
+
+};
+
+setupAndRunServer();
